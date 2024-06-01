61327a030d5573b185867da0ac6d9ae7e87a8f8e
Alexander Ortiz Alonso <alexluthor68@gmail.com>
2024-05-31 19:48:49 -0500

    Para ubicar los elementos de la Navbar en la aplicación se le dan estilos a las etiquetas <nav> y <ul>; a lo cual se les crean clases, con diferentes estilos en donde se lograr definir 2 estructuras de elementos, en la parte izquierda y derecha, y a la vez ubicar los elementos uno al lado de otro y en sentido horizontal; igualmente darle estilos a la fuente

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <nav className='flex justify-between items-center fixed z-10'>
+        <nav className='flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light'>
             <ul className='flex items-center'>
                 <li>
                     <NavLink to='/'>
