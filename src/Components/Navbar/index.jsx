875d0819b0b0ef51fc1a73c3acc82f3dd43ca0c3
Alexander Ortiz Alonso <alexluthor68@gmail.com>
2024-06-03 13:25:11 -0500

    Se continua dando estilos a la Navbar, para separar los elementos uno de otro, tanto en la estructura izquierda como el de la derecha, se le agrega a las etiquetas 'ul' el comando: gap-3, se le da estilos a 'shopi'. Para que se resalten como en negrilla los elementos de la pagina, se le agrega codigo extraido de la documentacion de NavLink

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,61 +1,143 @@
 import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+    const activeStyle = 'underline underline-offset-4'
+    
+    
     return (
         <nav className='flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light'>
-            <ul className='flex items-center'>
-                <li>
-                    <NavLink to='/'>
+            <ul className='flex items-center gap-3'>
+                <li className='font-semibold text-lg'>
+                    <NavLink to='/'
+                        style={({ isActive, isPending, isTransitioning }) => {
+    return {
+    fontWeight: isActive ? "bold" : "",
+    color: isPending ? "red" : "black",
+    viewTransitionName: isTransitioning ? "slide" : "",
+    };
+    }}>
                         Shopi
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/'>
+                    <NavLink
+                        to='/'
+                        style={({ isActive, isPending, isTransitioning }) => {
+    return {
+      fontWeight: isActive ? "bold" : "",
+      color: isPending ? "red" : "black",
+      viewTransitionName: isTransitioning ? "slide" : "",
+    };
+  }}>
                         All
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/clothes'>
+                    <NavLink
+                        to='/clothes'
+                        style={({ isActive, isPending, isTransitioning }) => {
+    return {
+      fontWeight: isActive ? "bold" : "",
+      color: isPending ? "red" : "black",
+      viewTransitionName: isTransitioning ? "slide" : "",
+    };
+  }}>
                         Clothes
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/electronics'>
+                    <NavLink
+                        to='/electronics'
+                        style={({ isActive, isPending, isTransitioning }) => {
+    return {
+      fontWeight: isActive ? "bold" : "",
+      color: isPending ? "red" : "black",
+      viewTransitionName: isTransitioning ? "slide" : "",
+    };
+  }}>
                         Electronics
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/furniture'>
+                    <NavLink
+                        to='/furniture'
+                        style={({ isActive, isPending, isTransitioning }) => {
+    return {
+      fontWeight: isActive ? "bold" : "",
+      color: isPending ? "red" : "black",
+      viewTransitionName: isTransitioning ? "slide" : "",
+    };
+  }}>
                         Furniture
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/toys'>
+                    <NavLink
+                        to='/toys'
+                        style={({ isActive, isPending, isTransitioning }) => {
+    return {
+      fontWeight: isActive ? "bold" : "",
+      color: isPending ? "red" : "black",
+      viewTransitionName: isTransitioning ? "slide" : "",
+    };
+  }}>
                         Toys
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/others'>
+                    <NavLink
+                        to='/others'
+                        style={({ isActive, isPending, isTransitioning }) => {
+    return {
+      fontWeight: isActive ? "bold" : "",
+      color: isPending ? "red" : "black",
+      viewTransitionName: isTransitioning ? "slide" : "",
+    };
+  }}>
                         Others
                     </NavLink>
                 </li>
             </ul>
-            <ul className='flex items-center'>
-                <li>
+            <ul className='flex items-center gap-3'>
+                <li className='text-black/40'>
                     alexlutor1968@gmail.com
                 </li>
                 <li>
-                    <NavLink to='/my-orders'>
+                    <NavLink
+                        to='/my-orders'
+                        style={({ isActive, isPending, isTransitioning }) => {
+    return {
+      fontWeight: isActive ? "bold" : "",
+      color: isPending ? "red" : "black",
+      viewTransitionName: isTransitioning ? "slide" : "",
+    };
+  }}>
                         My Orders
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/my-account'>
+                    <NavLink
+                        to='/my-account'
+                        style={({ isActive, isPending, isTransitioning }) => {
+    return {
+      fontWeight: isActive ? "bold" : "",
+      color: isPending ? "red" : "black",
+      viewTransitionName: isTransitioning ? "slide" : "",
+    };
+  }}>
                         My Account
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/sign-in'>
+                    <NavLink
+                        to='/sign-in'
+                        style={({ isActive, isPending, isTransitioning }) => {
+    return {
+      fontWeight: isActive ? "bold" : "",
+      color: isPending ? "red" : "black",
+      viewTransitionName: isTransitioning ? "slide" : "",
+    };
+  }}>
                         Sign In
                     </NavLink>
                 </li>
