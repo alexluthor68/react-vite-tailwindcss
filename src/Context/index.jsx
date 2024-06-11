14172818341748bfa580e5666985f5034adf6c0a
Alexander Ortiz Alonso <alexluthor68@gmail.com>
2024-06-11 14:34:35 -0500

    Mostrando productos en 'ProductDetail', en 'Context' se crea un nuevo estado y tambien se organizan los otros estados creados

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,10 +3,14 @@ import { createContext, useState } from 'react'
 export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvider = ({ children }) => {
+    // Shopping Cart --> Increment quantity.
     const [count, setCount] = useState(0)
+    // Product Detail --> Open/Close.
     const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
     const openProductDetail = () => setIsProductDetailOpen(true)
     const closeProductDetail = () => setIsProductDetailOpen(false)
+    // Product Details --> Show product.
+    const [productToShow, setProductToShow] = useState({})
 
     return (
         <ShoppingCartContext.Provider value={{
@@ -14,7 +18,9 @@ export const ShoppingCartProvider = ({ children }) => {
             setCount,
             openProductDetail,
             closeProductDetail,
-            isProductDetailOpen
+            isProductDetailOpen,
+            productToShow,
+            setProductToShow
         }}>
             {children}
         </ShoppingCartContext.Provider>        
