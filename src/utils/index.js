830875bd10d222ecf1022fd1488d7c8f285ad6be
Alexander Ortiz Alonso <alexluthor68@gmail.com>
2024-06-20 12:23:19 -0500

    Clase 20 Suma total de productos en el carrito, se crea una carpeta en src de nombre 'utils', a dentro de ella se crea un archivo 'index.js'; pero sin la 'x', se modifican los componentes: 'CheckoutSideMenu/index.jsx' y 'utils/index.js'

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,10 @@
-export const totalPrice = () => {
-    
+/*
+This function calculates total price of a new order.
+@param {Array} products ==> CartProducts: Array of objects.
+@returns {number} ==> Total price.
+*/
+export const totalPrice = (products) => {
+    let sum = 0
+    products.forEach(product => sum += product.price)
+    return sum
 }
