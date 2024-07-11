ae56676b23d9dba397420a2c65907beee80d6f42
Alexander Ortiz Alonso <alexluthor68@gmail.com>
2024-07-10 19:01:32 -0500

    Clase 25 Reto: ordenes de compra con TailWindCSS, se soluciono por completo el reto de configurar los estilos que hacian falta en OrdersCard y MyOrders

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -1,17 +1,20 @@
-import { XMarkIcon } from '@heroicons/react/24/solid'
+import { ChevronRightIcon } from '@heroicons/react/24/solid'
 
 const OrdersCard = props => {
     const { totalPrice, totalProducts } = props    
 
     return (
         <div className='flex justify-between items-center mb-4 border border-black rounded-lg w-80 p-4'>            
-            <p className='flex justify-between w-full'>
-                <div className='flex flex-col'>
+            <div className='flex justify-between w-full'>
+                <p className='flex flex-col'>
                     <span className='font-light'>24.06.24</span>
                     <span className='font-light'>{totalProducts} articles</span>
-                </div>                
-                <span className='font-medium text-2xl'>${totalPrice}</span>
-            </p>
+                </p>
+                <p className='flex items-center gap-2'>
+                    <span className='font-medium text-2xl'>${totalPrice}</span>
+                    <ChevronRightIcon className='h-6 w-6 text-black cursor-pointer' />
+                </p>                
+            </div>
         </div>        
     )
 }
