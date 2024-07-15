1c6c0e3d54757f358493e5edce525da32310d90e
Alexander Ortiz Alonso <alexluthor68@gmail.com>
2024-07-15 12:27:13 -0500

    Clase 28 Filtrando categorias con JavaScript, se terminaron de modificar los archivos de App y Context

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -28,24 +28,46 @@ export const ShoppingCartProvider = ({ children }) => {
     //10. Get products by category.
     const [searchByCategory, setSearchByCategory] = useState(null)
     
-
-     useEffect(() => {
-      fetch('https://api.escuelajs.co/api/v1/products')
-         .then(response => response.json())
-         .then(data => setItems(data))
-     }, [])
+    
+    useEffect(() => {
+        fetch('https://api.escuelajs.co/api/v1/products')
+        .then(response => response.json())
+        .then(data => setItems(data))
+    }, [])
     
     const filteredItemsByTitle = (items, searchByTitle) => {
         return items?.filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
     }
-     const filteredItemsByCategory = (items, searchByCategory) => {
-        return items?.filter(item => item.category.toLowerCase().includes(searchByCategory.toLowerCase()))
+    const filteredItemsByCategory = (items, searchByCategory) => {
+        console.log('ITEMS: ', items)
+        return items?.filter(item => item.category.name.toLowerCase().includes(searchByCategory.toLowerCase()))
     }
 
-    useEffect(() => {  
-        if (searchByTitle) setFilteredItems(filteredItemsByTitle(items, searchByTitle))
-    }, [items, searchByTitle])   
-    
+    const filterBy = (searchType, items, searchByTitle, searchByCategory) => {
+    if (searchType === 'BY_TITLE') {
+      return filteredItemsByTitle(items, searchByTitle)
+    }
+
+    if (searchType === 'BY_CATEGORY') {
+      return filteredItemsByCategory(items, searchByCategory)
+    }
+
+    if (searchType === 'BY_TITLE_AND_CATEGORY') {
+      return filteredItemsByCategory(items, searchByCategory).filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+    }
+
+    if (!searchType) {
+      return items
+    }
+  }
+
+  useEffect(() => {
+    if (searchByTitle && searchByCategory) setFilteredItems(filterBy('BY_TITLE_AND_CATEGORY', items, searchByTitle, searchByCategory))
+    if (searchByTitle && !searchByCategory) setFilteredItems(filterBy('BY_TITLE', items, searchByTitle, searchByCategory))
+    if (!searchByTitle && searchByCategory) setFilteredItems(filterBy('BY_CATEGORY', items, searchByTitle, searchByCategory))
+    if (!searchByTitle && !searchByCategory) setFilteredItems(filterBy(null, items, searchByTitle, searchByCategory))
+  }, [items, searchByTitle, searchByCategory])   
+        
     return (
         <ShoppingCartContext.Provider value={{
             count,
